dfcf7bde2c522291ed322f7f7c8a6a868cb30369
noonquiver <camilo.hernandez25@hotmail.com>
2022-08-01 18:49:51 -0500

    first commit

---
+++ Detect-a-Beacon/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Detect-a-Beacon
+//
+//  Created by Camilo Hernández Guerrero on 1/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
