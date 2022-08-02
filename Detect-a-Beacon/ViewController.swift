329ce703157d0cfa27a8766dec2715eab13d42c8
noonquiver <camilo.hernandez25@hotmail.com>
2022-08-01 19:36:33 -0500

    UI now updates on beacon proximity.

--- Detect-a-Beacon/ViewController.swift
+++ Detect-a-Beacon/ViewController.swift
@@ -26,11 +26,45 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         if manager.authorizationStatus == .authorizedAlways {
             if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
                 if CLLocationManager.isRangingAvailable() {
-                    //do stuff
+                    startScanning()
                 }
             }
         }
     }
 
+    func startScanning() {
+        let uuid = UUID(uuidString: "5A4BCFCE-174E-4BAC-A814-092E77F6B7E5")!
+        let beaconRegion = CLBeaconRegion(uuid: uuid, major: 123, minor: 456, identifier: "MyBeacon")
+        
+        locationManager?.startMonitoring(for: beaconRegion)
+        locationManager?.startRangingBeacons(satisfying: beaconRegion.beaconIdentityConstraint)
+    }
+    
+    func update(distance: CLProximity) {
+        UIView.animate(withDuration: 1) {
+            switch distance {
+            case .immediate:
+                self.view.backgroundColor = .red
+                self.distanceReading.text = "RIGHT HERE"
+            case .near:
+                self.view.backgroundColor = .orange
+                self.distanceReading.text = "NEAR"
+            case .far:
+                self.view.backgroundColor = .blue
+                self.distanceReading.text = "FAR"
+            default:
+                self.view.backgroundColor = .gray
+                self.distanceReading.text = "UNKNOWN"
+            }
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didRange beacons: [CLBeacon], satisfying beaconConstraint: CLBeaconIdentityConstraint) {
+        if let beacon = beacons.first {
+            update(distance: beacon.proximity)
+        } else {
+            update(distance: .unknown)
+        }
+    }
 }
 
